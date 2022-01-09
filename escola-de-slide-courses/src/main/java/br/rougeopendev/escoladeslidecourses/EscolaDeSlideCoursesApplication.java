dd781be5b1fb47cf354d19d2578babced2e46774
Gustavo Amorim <gustavoframorim@hotmail.com>
2022-01-09 09:02:58 -0300

    initial commit

---
+++ escola-de-slide-courses/src/main/java/br/rougeopendev/escoladeslidecourses/EscolaDeSlideCoursesApplication.java
@@ -0,0 +1,13 @@
+package br.rougeopendev.escoladeslidecourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaDeSlideCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaDeSlideCoursesApplication.class, args);
+	}
+
+}
---
+++ escola-de-slide-courses/src/test/java/br/rougeopendev/escoladeslidecourses/EscolaDeSlideCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rougeopendev.escoladeslidecourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaDeSlideCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
